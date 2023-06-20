4f663cbfa22861ccf503e3e561bfd55d6df6e2bb
Gavin Lee <GavinLee@ecs.corp>
2023-06-20 15:57:59 +0800

    test

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+## this is the stage one , also know as the build step
+FROM node:12.17.0-alpine
+
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+COPY package.json /usr/src/app/
+
+RUN npm install
+    
+COPY . /usr/src/app
+
+EXPOSE 8080
+
+CMD [ "npm", "run", "start" ]
---
+++ app.js
@@ -0,0 +1,78 @@
+'use strict';
+
+const express = require('express');
+var request = require('request');
+var cors = require('cors');
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+var gReqBody = {}
+
+var eventGridHook = function(req, res) {
+
+  //console.dir(req.body);
+
+  console.log('JavaScript HTTP trigger function begun');
+  var validationEventType = "Microsoft.EventGrid.SubscriptionValidationEvent";
+
+  for (var events in req.body) {
+      var body = req.body[events];
+      console.log('body:' + events);
+      //console.dir(body);
+      // Deserialize the event data into the appropriate type based on event type
+      if (body.data && body.eventType == validationEventType) {
+          console.log("Got SubscriptionValidation event data, validation code: " + body.data.validationCode + " topic: " + body.topic);
+          // Do any additional validation (as required) and then return back the below response
+          var code = body.data.validationCode;
+          res.json({ status: 200, body: { "ValidationResponse": code } });
+          res.status = 200;
+          res.body = { "ValidationResponse": code };
+          //appEventBus.emit('appHook', body);
+
+    
+          //post back //這是一個必要的動作
+          var validationUrl = body.data.validationUrl;
+          request(validationUrl, function(error, response, body) {
+              console.log('error:', error); // Print the error if one occurred
+              console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+              console.log('body:', body); // Print the HTML for the Google homepage.
+          });
+
+      } else if (body.data) {
+        console.log(body.data.body)
+        var msgOutput = Buffer(body.data.body,'base64').toString()
+        console.dir(msgOutput);
+        //gReqBody = body.data;
+        gReqBody = msgOutput;
+    }
+  }
+  res.end();
+};
+
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+)
+
+app.use(express.json())
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send(gReqBody)
+    // res.send('Hello World');
+  });
+app.post('/api/GavinEdgeX', (req, res) => {
+  console.log(req.body);
+  gReqBody = req.body;
+  res.send(req.body);
+})
+
+app.post('/iothub/wemos_d1', eventGridHook);
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);   
