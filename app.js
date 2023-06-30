a7a0d3740f67f4af1133ee8287b1acf83dbc7b2e
Gavin Lee <GavinLee@ecs.corp>
2023-06-30 13:31:07 +0800

    refactor 2

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 const express = require("express");
 var ip = require("ip");
 var http = require("http");
-var request = require("request");
 var cors = require("cors");
 var moment = require("moment-timezone");
 var mqttClient = require('./lib/iothub_client')
@@ -21,39 +20,11 @@ var gReqBody = {};
 var server = http.createServer(app);
 var eventGridHook = function (req, res) {
   try {
-    //console.dir(req.body);
-
-    console.log("JavaScript HTTP trigger function begun");
-    var validationEventType = "Microsoft.EventGrid.SubscriptionValidationEvent";
-
 
     for (var events in req.body) {
       var body = req.body[events];
-      console.log("body:" + events);
-      //console.dir(body);
-      // Deserialize the event data into the appropriate type based on event type
-      if (body.data && body.eventType == validationEventType) {
-        console.log(
-          "Got SubscriptionValidation event data, validation code: " +
-          body.data.validationCode +
-          " topic: " +
-          body.topic
-        );
-        // Do any additional validation (as required) and then return back the below response
-        var code = body.data.validationCode;
-        res.json({ status: 200, body: { ValidationResponse: code } });
-        res.status = 200;
-        res.body = { ValidationResponse: code };
-        //appEventBus.emit('appHook', body);
-
-        //post back //這是一個必要的動作
-        // var validationUrl = body.data.validationUrl;
-        // request(validationUrl, function (error, response, body) {
-        //   console.log("error:", error); // Print the error if one occurred
-        //   console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-        //   console.log("body:", body); // Print the HTML for the Google homepage.
-        // });
-      } else if (body.data) {
+      console.log("body:" + events); 
+      if (body.data) {
         console.log("body.data", body.data);
         if (body.data.body) {
           var msgOutput = Buffer(body.data.body, "base64").toString();
@@ -86,18 +57,7 @@ app.get("/", (req, res) => {
   try {
     res.status(200).send(gReqBody);
   } catch (error) {
-    console.log("root error:", error);
-  }
-  // res.send('Hello World');
-});
-
-app.post("/api/GavinEdgeX", (req, res) => {
-  try {
-    console.log(req.body);
-    gReqBody = req.body;
-    res.status(200).send(req.body);
-  } catch (error) {
-    console.log("GavinEdgeX error:", error);
+    console.log("wemos_d1 error: ", error);
   }
 });
 
@@ -108,7 +68,7 @@ app.post("/api/changefanspeed", async (req, res) => {
     console.log(response);
     res.send(response);
   } catch (error) {
-    console.log("GavinEdgeX error:", error);
+    console.log("wemos_d1 error: ", error);
   }
 });
 
@@ -119,7 +79,7 @@ app.post("/api/changemode", async (req, res) => {
     console.log(response);
     res.send(response);
   } catch (error) {
-    console.log("GavinEdgeX error:", error);
+    console.log("wemos_d1 error: ", error);
   }
 });
 
